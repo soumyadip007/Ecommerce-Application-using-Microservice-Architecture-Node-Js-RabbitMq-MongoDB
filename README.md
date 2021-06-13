bf38342cbbc2931bd16f0679cdc50255f0949a3c
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-06-13 17:05:47 +0530

    Flow Added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Ecommerce-Application-using-Microservice-Architecture-Node-Js-RabbitMq-MongoDB
+
+### Services:
+  1. Auth Service
+  2. Product Service
+  3. Order Service
+
+<img src="../flow1" >
+<img src="../flow2" >
--- product-service/index.js
+++ product-service/index.js
@@ -1,13 +1,17 @@
+  
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT_ONE || 8080;
 const mongoose = require("mongoose");
+const Product = require("./Product");
 const jwt = require("jsonwebtoken");
 const amqp = require("amqplib");
+const isAuthenticated = require("../isAuthenticated");
 var order;
 
 var channel, connection;
 
+app.use(express.json());
 mongoose.connect(
     "mongodb://localhost/product-service",
     {
@@ -15,9 +19,10 @@ mongoose.connect(
         useUnifiedTopology: true,
     },
     () => {
-        console.log(`Product DB Connected`);
+        console.log(`Product-Service DB Connected`);
     }
 );
+
 
 
 app.use(express.json());
@@ -33,3 +38,50 @@ async function connect() {
     await channel.assertQueue("PRODUCT");
 }
 connect();
+
+//Create a New Product
+
+app.post("/product/create", isAuthenticated, async(req,res)=>{
+    const { name, description, price } = req.body;
+    const newProduct = new Product({
+        name,
+        description,
+        price,
+    });
+    newProduct.save();
+    return res.json(newProduct);
+});
+
+//Buy a Product
+app.post("/product/buy", isAuthenticated, async (req, res) => {
+    const { ids } = req.body;
+    const products = await Product.find({ _id: { $in: ids } });
+    channel.sendToQueue(
+        "ORDER",
+        Buffer.from(
+            JSON.stringify({
+                products,
+                userEmail: req.user.email,
+            })
+        )
+    );
+    channel.consume("PRODUCT", (data) => {
+        order = JSON.parse(data.content);
+    });
+    return res.json(order);
+});
+
+/*
+curl --location --request POST 'http://localhost:8080/product/create' \
+--header 'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNvdW15YWRpcC5ub3RlQGdtYWlsLmNvbSIsIm5hbWUiOiJTb3VteWFkaXAiLCJpYXQiOjE2MjM1ODMxNDh9.lIz7HedpdammEJYqrIjXrW1mcXW_C7y9j6dcHjuCFUY' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "name": "product1",
+    "description": "product1 desc",
+    "price":1000
+}'
+
+
+
+
+*/
